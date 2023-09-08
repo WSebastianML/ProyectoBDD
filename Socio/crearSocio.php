2636402a9d03810fd17c9016ad95e8c481f5ee60
Sebastian <wsebastianmoyano@gmail.com>
2023-09-08 15:16:37 -0500

    CRUD para Socios

---
+++ Socio/crearSocio.php
@@ -0,0 +1,102 @@
+<?php
+require '../conexion.php';
+$con = new Conexion();
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id = $_POST["socio_id"];
+    $cc = $_POST["cc"];
+    $nombre = $_POST["nombre"];
+    $apellido = $_POST["apellido"];
+    $direccion = $_POST["direccion"];
+    $telefono = $_POST['telefono'];
+    $avaladoPor = $_POST['avaladoPor'];
+    $agencia_id = $_POST['agencia_id'];
+
+    if($agencia_id == 1){
+        $consulta = "INSERT INTO Socios_01 VALUES ($id, $cc, '$nombre', '$apellido', '$direccion', $telefono, $avaladoPor, $agencia_id)";
+        //echo $consulta;
+        $query = $con->getConexion()->prepare($consulta);
+        $query->execute();
+        header('Location: socio.php');
+
+        if(!$query){
+            echo "Error";
+        }
+    } else if($agencia_id == 2){
+        $consulta = "INSERT INTO [WIN-D5TV30MDGGJ].[BaseGuayaquil].dbo.Socios_02 VALUES ($id, $cc, '$nombre', '$apellido', '$direccion', $telefono, $avaladoPor, $agencia_id)";
+        //echo $consulta;
+        $query = $con->getConexion()->prepare($consulta);
+        $query->execute();
+        header('Location: socio.php');
+    }
+
+    
+}else{
+    echo "no";
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Glob Gusters</title>
+    <link rel="stylesheet" href="../interfaz.css">
+</head>
+<body>
+    <header>
+        <h1>Glob Gusters</h1>
+    </header>
+
+    <main class="contenedor">
+        <h2>Crear un nuevo socio</h2>
+        <a href="socio.php">Regresar</a>
+        <form method="POST">
+            <fieldset>
+                <legend>Ingrese los datos</legend>
+                <div>
+                    <label for="socio_id">ID</label> 
+                    <input type="text" name="socio_id">
+                </div>
+
+                <div>
+                    <label for="cc">Cedula</label> 
+                    <input type="text" name="cc">
+                </div>
+
+                <div>
+                    <label for="nombre">Nombre</label> 
+                    <input type="text" name="nombre">
+                </div>
+
+                <div>
+                    <label for="apellido">Apellido</label> 
+                    <input type="text" name="apellido">
+                </div>
+
+                <div>
+                    <label for="direccion">Direccion</label> 
+                    <input type="text" name="direccion"> 
+                </div>
+
+                <div>
+                    <label for="telefono">Telefono</label> 
+                    <input type="text" name="telefono"> 
+                </div>
+
+                <div>
+                    <label for="avaladoPor">Avalado Por</label> 
+                    <input type="text" name="avaladoPor"> 
+                </div>
+
+                <div>
+                    <label for="agencia_id">Agencia</label> 
+                    <input type="text" name="agencia_id"> 
+                </div>
+            </fieldset> 
+            <input type="submit" value="Aceptar"> 
+        </form> 
+    </main> 
+</body> 
+</html> 
---
+++ Socio/socio.php
@@ -0,0 +1,96 @@
+<?php
+require '../conexion.php';
+$con = new Conexion();
+$consulta = "SELECT * FROM Socio";
+$query = $con->getConexion()->prepare($consulta);
+$query->execute();
+if(!$query){
+    echo "Error";
+}
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $socioId = $_POST['socio_id'];
+    $agencia_id = $_POST['agencia_id'];
+    
+    if($agencia_id == 1){
+        $borrar = "DELETE Socios_01 WHERE socio_id = $socioId";
+        $query = $con->getConexion()->prepare($borrar);
+        $query->execute();
+
+    }else if($agencia_id == 2){
+        $borrar = "DELETE [WIN-D5TV30MDGGJ].[BaseGuayaquil].dbo.Socios_02 WHERE socio_id = $socioId";
+        $query = $con->getConexion()->prepare($borrar);
+        $query->execute();
+    }
+    
+    
+    //echo $borrar;
+    header('Location: socio.php');
+
+    if(!$query){
+        echo "Error";
+    }
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Socios</title>
+    <link rel="stylesheet" href="../interfaz.css">
+</head>
+<body>
+
+    <main class="contenedor">
+        <h1>Socios</h1>
+        <a href="crearSocio.php">Crear</a>
+        <a href="actualizarSocio.php">Actualizar</a>
+        <table class="propiedades">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Cedula</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Direccion</th>
+                    <th>Telefono</th>
+                    <th>Avalado Por</th>
+                    <th>Agencia</th>
+                    <th>Operaciones</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php
+                
+                while($row = $query->fetch(PDO::FETCH_ASSOC)){
+                    echo "<tr>";
+                    echo "<td>" . $row['socio_id'] . "</td>";
+                    echo "<td>" . $row['cc'] . "</td>";
+                    echo "<td>" . $row['nombre'] . "</td>";
+                    echo "<td>" . $row['apellido'] . "</td>";
+                    echo "<td>" . $row['direccion'] . "</td>";
+                    echo "<td>" . $row['telefono'] . "</td>";
+                    echo "<td>" . $row['avaladoPor'] . "</td>";
+                    echo "<td>" . $row['agencia_id'] . "</td>";
+                    echo "<td>";
+                    echo "<form method='POST'>";
+                    echo "<input type='hidden' name='socio_id' value='" . $row['socio_id'] . "'>";
+                    echo "<input type='hidden' name='agencia_id' value='" . $row['agencia_id'] . "'>";
+                    echo "<input type='submit' value='Borrar'>";
+                    echo "</form>";
+                    echo "</td>";
+                    echo "</tr>";
+                }
+                
+                ?>
+            </tbody>
+        </table>
+    </main>
+    
+</body>
+</html>
